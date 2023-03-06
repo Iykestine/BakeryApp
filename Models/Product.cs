f51f77ba575c3f697df8fa90917a14f4ff2a5fad
Iykestine <roboticike@yahoo.com>
2023-03-06 15:06:19 +0100

    Revert "Created First Database Migration"
    This reverts commit ccd3a120c0024d73f395c12edfac843c05015e41.

--- Models/Product.cs
+++ Models/Product.cs
@@ -7,10 +7,5 @@ namespace BakeryApp.Models
 {
     public class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public string ImageName { get; set; }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,7 +8,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BakeryApp.Data;
 
 namespace BakeryApp
 {
@@ -25,7 +24,6 @@ namespace BakeryApp
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
-            services.AddDbContext<BakeryContext>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
