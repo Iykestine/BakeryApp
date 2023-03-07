7310f154e242a71598cb30e780efccbe049fad40
Iykestine <roboticike@yahoo.com>
2023-03-07 12:56:56 +0100

    Added Form to fulfill Orders with client-side validation

--- Pages/Order.cshtml.cs
+++ Pages/Order.cshtml.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using BakeryApp.Data;
@@ -16,7 +17,26 @@ namespace BakeryApp.Pages
         [BindProperty(SupportsGet = true)]
         public int Id { get; set; }
         public Product Product { get; set; }
+        [BindProperty, EmailAddress, Required, Display(Name = "Your Email Address")]
+        public string OrderEmail { get; set; }
+        [BindProperty, Required(ErrorMessage = "Please supply a shipping address"), Display(Name = "Shipping Address")]
+        public string OrderShipping { get; set; }
+        [BindProperty, Display(Name = "Quantity")]
+        public int OrderQuantity { get; set; } = 1;
+
+        // to take Orders
         public async Task OnGetAsync() => Product = await db.Products.FindAsync(Id);
+
+        //To process Orders
+        public async Task<IActionResult> OnPostAsync()
+        {
+            Product = await db.Products.FindAsync(Id);
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("OrderSuccess");
+            }
+            return Page();
+        }
     }
     
 }
